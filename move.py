1b36beba9a664175f01a947f681e89a8c84edb46
Rachit985 <142338686+Rachit985@users.noreply.github.com>
2024-11-18 22:00:11 +0530

    Add files via upload

--- move.py
+++ move.py
@@ -1,10 +1,27 @@
 import os
 class move:
-    def __init__(self,filepath):
+    def __init__(self,filepath): #constructor function
         self.filepath=filepath
-    files = [f for f in os.listdir(.filepath) if os.path.isfile(os.path.join(filepath,f))]
- 
-    print(os.path.join(filepath,"pdf","fe.txt"))
-    def pdf(file):
-        x = list(locals().keys())
-        print(x[0])
+    
+    def file_names(self):  #getting file names 
+        self.filenames=os.listdir(self.filepath)
+
+    sorted_files={'folders':[]}
+
+    def sortfiles(self):  #sorting files by type
+        for i in self.filenames:
+            if not os.path.isfile(os.path.join(self.filepath,i)):
+                self.sorted_files['folders'].append(i)
+            else:
+                split_up_name = os.path.splitext(i)
+                extenstion=split_up_name[1][1:]
+                if extenstion in self.sorted_files:
+                    self.sorted_files[extenstion].append(i)
+                else:
+                    self.sorted_files[extenstion]=[]
+        print(self.sorted_files)
+    
+
+test = move(r"C:\Users\rachi\OneDrive\Desktop")
+test.file_names()
+test.sortfiles()
